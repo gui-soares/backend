bca7b291bb7cff15fa8ccd65bfac5b69fdde3a63
Guilherme <eng.guilhermemsoares@gmail.com>
2019-04-10 00:03:47 -0300

    Estrutura back-end

---
+++ src/server.js
@@ -0,0 +1,35 @@
+const express = require ('express');
+const mongoose = require ('mongoose');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', function socket() {
+	socket.on('connectRoom', function box(){
+		socket.join(box);
+	})
+});
+
+
+mongoose.connect('mongodb+srv://guilherme:bosque4550@app-rocketseat-vp9uo.mongodb.net/omnistack?retryWrites=true',
+{
+  useNewUrlParser: true
+});
+
+app.use( function(req,res,next){
+	req.io = io;
+	return next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/files', express.static(path.resolve(__dirname,'..','tmp')));
+
+app.use(require('./routes'));
+
+server.listen(3333);
